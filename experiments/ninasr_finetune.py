25fda41b559b014dac5d050dbf8df054cec419f8
jkubiak <jkubiak@man.poznan.pl>
2025-10-24 18:49:55 +0200

    feat: aim logger workflow

--- experiments/ninasr_finetune.py
+++ experiments/ninasr_finetune.py
@@ -3,22 +3,30 @@ from typing import cast
 
 from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
+from torchsr.models import ninasr_b0
 
-from models.fsrcnn.fsrcnn import FSRCNN_WITHOUT_PRETRAIN
 from src.dataset import QuakeDataset
+from src.logger import setup_aim_logger
 from src.trainer import Trainer
 
 if __name__ == "__main__":
     os.makedirs("checkpoints", exist_ok=True)
     config = cast(DictConfig, OmegaConf.load("configs/config.yaml"))
 
+    experiment_tracker = setup_aim_logger("ninasr_finetune_4x_experiment", config)
+
     train_dataset = QuakeDataset(config, split="train")
     train_dataloader = DataLoader(train_dataset, **config.train_dataloader)
 
     val_dataset = QuakeDataset(config, split="val")
     val_dataloader = DataLoader(val_dataset, **config.val_dataloader)
-    print(f"Scale: {config.training.scale}")
-    model = FSRCNN_WITHOUT_PRETRAIN(scale=config.training.scale)
+    model = ninasr_b0(scale=4, pretrained=True)
 
-    trainer = Trainer(model, train_dataloader, val_dataloader, config)
+    trainer = Trainer(
+        model=model,
+        train_loader=train_dataloader,
+        val_loader=val_dataloader,
+        config=config,
+        experiment_tracker=experiment_tracker,
+    )
     trainer.fit()
--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,8 @@
 import logging
 
+from aim import Run
+from omegaconf import DictConfig
+
 
 def setup_logger(name: str, level=logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger(name)
@@ -13,6 +16,18 @@ def setup_logger(name: str, level=logging.DEBUG) -> logging.Logger:
         logger.addHandler(console_handler)
 
     return logger
+
+
+def setup_aim_logger(name: str, config: DictConfig) -> Run:
+    import aim
+
+    experiment_tracker = aim.Run(
+        experiment=name,
+        repo="aim://server.aim.mjanicki.org:53800",
+    )
+    experiment_tracker['config'] = config
+
+    return experiment_tracker
 
 
 logger = setup_logger(__name__)
